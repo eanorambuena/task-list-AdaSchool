6a8d588e6c58314bdf6cbb770cd814a627182e69
Emmanuel Norambuena <eanorambuena@uc.cl>
2022-11-12 02:40:29 -0300

    Complete review-1

--- src/App.js
+++ src/App.js
@@ -14,7 +14,3 @@ function App() {
 }
 
 export default App;
-/* implementa el hook useState para controlar el listado de tareas.
-Así mismo cada tarea debe tener un estado de completada o pendiente, utiliza valores booleanos.
-Muestra un listado estático con algunas tareas de ejemplo.
-Publica tu código en Github, en tu repositorio task-list crea un branch llamado project-2 y compártelo con nosotros. 👍🏼⬇️ */
